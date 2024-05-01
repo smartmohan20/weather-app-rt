715a0a74e50f08f2f39d57c9afe1b55e0e188bf3
smartmohan20 <dev.smartmohan@gmail.com~>
2024-05-01 15:08:29 +0530

    Installed styled-components library and used poppins font style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import GlobalStyle from './fonts/GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
